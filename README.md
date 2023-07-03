9d23a648f6412f16d379ed7515e5ce1e4c1220dc
ANIMATORIMAGINATION <108915745+ANIMATORIMAGINATION@users.noreply.github.com>
2023-07-02 23:48:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-dp5keo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-dp5keo)
---
+++ script.js
@@ -0,0 +1,25 @@
+// File: script.js
+
+// Function to calculate the sum of two numbers
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+// Function to display a greeting message
+function greet(name) {
+  console.log(`Hello, ${name}! Welcome to the website.`);
+}
+
+// Function to generate a random number between a given range
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Example usage of the functions
+let result = calculateSum(5, 10);
+console.log(`The sum is: ${result}`);
+
+greet("doge");
+
+let randomNumber = generateRandomNumber(1, 100);
+console.log(`Generated random number: ${randomNumber}`);
